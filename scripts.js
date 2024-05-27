b4651b3be62ce0a336019c3d0267a778c7a4b7d9
Daewoong Lim <daewoong1025@gmail.com>
2024-05-27 16:46:23 +0900

    Complete board_filled function and place_tile function

--- scripts.js
+++ scripts.js
@@ -1,34 +1,31 @@
 // set initial board status
 function start_game(n) {
     let board = [];
-    for(let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         board.push(new Array(n).fill(0));
     };
-    coords = [];
-    for(let i = 0; i < 4; i++) {
-        coords.push(Math.floor(Math.random()*n));
-    };
-    while(coords[0] == coords[1] && coords[2] == coords[3]) {
-        coords[1] = Math.floor(Math.random()*n);
-        coords[3] = Math.floor(Math.random()*n);
-    };
-    board[coords[0]][coords[2]] = 2;
-    board[coords[1]][coords[3]] = 2;  
-    draw_board(board,n);  
+    board = place_tile(board, n);
+    board = place_tile(board, n);
+    draw_board(board, n);  
     return board;
 };
 
 // draw board on screen
 function draw_board(board, n) {
-    for(let i = 0; i < n; i++) {
-        for(let j = 0; j < n; j++) {
-            if(board[i][j] != 0) {
+    const b = document.getElementById('game-board');
+    while(b.hasChildNodes()) {
+        b.removeChild(b.firstChild);
+    };
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] != 0) {
                 var tile = document.createElement('div');
                 tile.setAttribute('class', 'tile');
                 tile.style.top = `${100 * i + 5}px`
                 tile.style.left = `${100 * j + 5}px`
                 tile.innerHTML = board[i][j];
-                document.getElementById('game-board').appendChild(tile);
+                b.appendChild(tile);
                 // console.log(i,j);
             };
         };
@@ -36,19 +33,71 @@ function draw_board(board, n) {
 };
 
 // move tiles
-function move(board, action) {
-    console.log(action)
-}
+function move(board, n, action) {
+    // console.log(action);
+    switch (action) {
+        case "w":
+            for (let i = 0; i < n; i++) {
+                for (let j = 0; j < n; j++) {
 
-let n = 4;
+                };
+            };
+            break;
+        case "s":
+            break;
+        case "a":
+            break;
+        case "d":
+            break;
+    }
+
+    board = place_tile(board, n);
+    draw_board(board, n);
+    if (board_filled(board, n)) {
+        console.log("game ended!");
+    };
+    return board;
+};
+
+// detects if board completely filled a.k.a. terminating condition
+function board_filled(board, n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] == 0) {
+                return false;
+            };
+        };
+    };
+    return true;
+};
+
+function place_tile(board, n) {
+    let empty = [];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] == 0) {
+                empty.push([i,j]);
+            };
+        };
+    };
+
+    const coords = empty[Math.floor(Math.random()*empty.length)];
+    board[coords[0]][coords[1]] = 2;
+    return board;
+};
+
+let n = 4; 
 let board = start_game(n);
 
 // event handler for moving tiles
 document.addEventListener('keyup', (e) => {
-    console.log(e.code);
+    if (e.code === "ArrowUp" || e.code === "KeyW") board = move(board, n, "w");
+    else if (e.code === "ArrowDown" || e.code === "KeyS") board = move(board, n, "s");
+    else if (e.code === "ArrowLeft" || e.code === "KeyA") board = move(board, n, "a");
+    else if (e.code === "ArrowRight" || e.code === "KeyD") board = move(board, n, "d");
+});
 
-    if (e.code === "ArrowUp" || e.code === "KeyW") move(board, "w");
-    else if (e.code === "ArrowDown" || e.code === "KeyS") move(board, "s");
-    else if (e.code === "ArrowLeft" || e.code === "KeyA") move(board, "a");
-    else if (e.code === "ArrowRight" || e.code === "KeyD") move(board, "d");
+// event handler for restart button
+document.getElementById("restart-button").addEventListener('click', () => {
+    board = start_game(n);
 });
