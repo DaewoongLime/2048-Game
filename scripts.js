ae94c89b4b5ab0b3dcd978d5284f190b9d2979d0
Daewoong Lim <daewoong1025@gmail.com>
2024-05-28 15:28:01 +0900

    Add button style

--- scripts.js
+++ scripts.js
@@ -9,7 +9,6 @@ function start_game(n) {
     };
     board = place_tile(board, n, true);
     board = place_tile(board, n, true);
-    board = [['2','4','8','16'],[32,64,128,256],[512,1024,1024,4096],[2,2,2,2]]
     draw_board(board, n);
     return board;
 };
